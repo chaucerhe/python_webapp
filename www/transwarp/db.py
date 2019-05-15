eff6d4e7b718b3c45d65abc8673a969660b5fe84
heziyi <heziyi1@126.com>
2019-05-15 16:17:48 +0800

    test

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -96,4 +96,103 @@ class _Engine(object):
         self._connect = connect
 
     def connect(self):
-        return self.connect()
+        return self._connect()
+
+
+def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
+    import mysql.connector
+    global engine
+    if engine is not None:
+        raise DBError('Engine is already initialized.')
+    params = dict(user=user, password=password, database=database, host=host, port=port)
+    defaults = dict(use_unicode=True, charset='utf8', collation='utf8_general_ci', autocommit=False)
+
+    for k, v in defaults.iteritems():
+        params[k] = kw.pop(k, v)
+    params.update(kw)
+    params['buffered'] = True
+    engine = _Engine(lambda: mysql.connector.connect(**params))
+    logging.info('Init mysql engine <%s> ok.' % hex(id(engine)))
+
+
+class _ConnectionCtx(object):
+    def __enter__(self):
+        global _db_ctx
+        self.should_cleanup = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_cleanup = True
+        return self
+
+    def __exit__(self, exctype, excvalue, traceback):
+        global _db_ctx
+        if self.should_cleanup:
+            _db_ctx.cleanup()
+
+    def connection():
+        return _ConnectionCtx()
+
+    def with_connection(func):
+        @functools.wraps(func)
+        def _wrapper(*args, ** kw):
+            with _ConnectionCtx():
+                return func(*args, **kw)
+        return _wrapper
+
+
+class _TransactionCtx(object):
+    def __enter__(self):
+        global _db_ctx
+        self.should_close_conn = False
+        if not _db_ctx.is_init():
+            _db_ctx.is_init()
+            self.should_close_conn = True
+        _db_ctx.transactions = _db_ctx.transactions + 1
+        logging.info('begin transaction...' if _db_ctx.transactions==1 else 'join current transaction...')
+        return self
+
+    def __exit__(self, exctype, excvalue, traceback):
+        global _db_ctx
+        _db_ctx.transactions = _db_ctx.transactions - 1
+        try:
+            if _db_ctx.transactions==0:
+                if exctype is None:
+                    self.commit()
+                else:
+                    self.rollback()
+        finally:
+            if self.should_close_conn:
+                _db_ctx.cleanup()
+
+    def commit(selfself):
+        global _db_ctx
+        logging.info('commit transaction..')
+        try:
+            _db_ctx.connection.commit()
+            logging.info('commit ok.')
+        except:
+            logging.warning('commit failed. try rollback...')
+            _db_ctx.connection.rollback()
+            logging.warning('rollback ok.')
+            raise
+
+    def rollback(self):
+        global _db_ctx
+        logging.warning('rollback transaction...')
+        _db_ctx.connection.rollback()
+        logging.info('rollback ok.')
+
+    def transaction(self):
+        return _TransactionCtx()
+
+    def with_transaction(func):
+        @functools.wraps(func)
+        def _wrapper(*args, **kw):
+            _start = time.time()
+            with _TransactionCtx():
+                return func(*args, **kw)
+            _profiling(_start)
+        return _wrapper
+
+    
+
